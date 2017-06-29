8cd1a00dcbc58d69b5bff2ed93a2f42d491e54cf
Mathis Rosenhauer <rosenhauer@dkrz.de>
2017-06-29 17:59:14 +0200

    cmake: fuzzing

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,77 +1,77 @@
-MACRO(CHECK_CLZLL VARIABLE)
-  CHECK_C_SOURCE_COMPILES(
+macro(check_clzll VARIABLE)
+  check_c_source_compiles(
     "int main(int argc, char *argv[])
 {return __builtin_clzll(1LL);}"
     ${VARIABLE}
     )
-ENDMACRO()
+endmacro()
 
-MACRO(CHECK_BSR64 VARIABLE)
-  CHECK_C_SOURCE_COMPILES(
+macro(check_bsr64 VARIABLE)
+  check_c_source_compiles(
     "int main(int argc, char *argv[])
 {unsigned long foo; unsigned __int64 bar=1LL;
 return _BitScanReverse64(&foo, bar);}"
     ${VARIABLE}
     )
-ENDMACRO()
+endmacro()
 
-MACRO(FIND_INLINE_KEYWORD)
+macro(find_inline_keyword)
   #Inspired from http://www.cmake.org/Wiki/CMakeTestInline
-  SET(INLINE_TEST_SRC "/* Inspired by autoconf's c.m4 */
+  set(INLINE_TEST_SRC "/* Inspired by autoconf's c.m4 */
 static inline int static_foo(){return 0\;}
 int main(int argc, char *argv[]){return 0\;}
 ")
-  FILE(WRITE ${CMAKE_CURRENT_BINARY_DIR}/CMakeTestCInline.c
+  file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/CMakeTestCInline.c
     ${INLINE_TEST_SRC})
 
-  FOREACH(KEYWORD "inline" "__inline__" "__inline")
-    IF(NOT DEFINED C_INLINE)
-      TRY_COMPILE(C_HAS_${KEYWORD}
+  foreach(KEYWORD "inline" "__inline__" "__inline")
+    if(NOT DEFINED C_INLINE)
+      try_compile(C_HAS_${KEYWORD}
         ${CMAKE_CURRENT_BINARY_DIR}
         ${CMAKE_CURRENT_BINARY_DIR}/CMakeTestCInline.c
         COMPILE_DEFINITIONS "-Dinline=${KEYWORD}"
         )
-      IF(C_HAS_${KEYWORD})
-        SET(C_INLINE TRUE)
-        ADD_DEFINITIONS("-Dinline=${KEYWORD}")
-        MESSAGE(STATUS "Inline keyword found - ${KEYWORD}")
-      ENDIF(C_HAS_${KEYWORD})
-    ENDIF(NOT DEFINED C_INLINE)
-  ENDFOREACH(KEYWORD)
+      if(C_HAS_${KEYWORD})
+        set(C_INLINE TRUE)
+        add_definitions("-Dinline=${KEYWORD}")
+        message(STATUS "Inline keyword found - ${KEYWORD}")
+      endif(C_HAS_${KEYWORD})
+    endif(NOT DEFINED C_INLINE)
+  endforeach(KEYWORD)
 
-  IF(NOT DEFINED C_INLINE)
-    ADD_DEFINITIONS("-Dinline=")
-    MESSAGE(STATUS "Inline keyword - not found")
-  ENDIF(NOT DEFINED C_INLINE)
-ENDMACRO(FIND_INLINE_KEYWORD)
+  if(NOT DEFINED C_INLINE)
+    add_definitions("-Dinline=")
+    message(STATUS "Inline keyword - not found")
+  endif(NOT DEFINED C_INLINE)
+endmacro(find_inline_keyword)
 
-MACRO(FIND_RESTRICT_KEYWORD)
-  SET(RESTRICT_TEST_SRC "/* Inspired by autoconf's c.m4 */
+macro(find_restrict_keyword)
+  set(RESTRICT_TEST_SRC "/* Inspired by autoconf's c.m4 */
 int foo (int * restrict ip){return ip[0]\;}
 int main(int argc, char *argv[]){int s[1]\;
 int * restrict t = s\; t[0] = 0\; return foo(t)\;}
 ")
 
-  FILE(WRITE ${CMAKE_CURRENT_BINARY_DIR}/CMakeTestCRestrict.c
+  file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/CMakeTestCRestrict.c
     ${RESTRICT_TEST_SRC})
 
-  FOREACH(KEYWORD "restrict" "__restrict" "__restrict__" "_Restrict")
-    IF(NOT DEFINED C_RESTRICT)
-      TRY_COMPILE(C_HAS_${KEYWORD}
+  foreach(KEYWORD "restrict" "__restrict" "__restrict__" "_Restrict")
+    if(NOT DEFINED C_RESTRICT)
+      try_compile(C_HAS_${KEYWORD}
         ${CMAKE_CURRENT_BINARY_DIR}
         ${CMAKE_CURRENT_BINARY_DIR}/CMakeTestCRestrict.c
         COMPILE_DEFINITIONS "-Drestrict=${KEYWORD}"
         )
-      IF(C_HAS_${KEYWORD})
-        SET(C_RESTRICT TRUE)
-        ADD_DEFINITIONS("-Drestrict=${KEYWORD}")
-        MESSAGE(STATUS "Restrict keyword found - ${KEYWORD}")
-      ENDIF(C_HAS_${KEYWORD})
-    ENDIF(NOT DEFINED C_RESTRICT)
-  ENDFOREACH(KEYWORD)
+      if(C_HAS_${KEYWORD})
+        set(C_RESTRICT TRUE)
+        add_definitions("-Drestrict=${KEYWORD}")
+        message(STATUS "Restrict keyword found - ${KEYWORD}")
+      endif(C_HAS_${KEYWORD})
+    endif(NOT DEFINED C_RESTRICT)
+  endforeach(KEYWORD)
 
-  IF(NOT DEFINED C_RESTRICT)
-    ADD_DEFINITIONS("-Drestrict=")
-    MESSAGE(STATUS "Restrict keyword - not found")
-  ENDIF(NOT DEFINED C_RESTRICT)
-ENDMACRO(FIND_RESTRICT_KEYWORD)
+  if(NOT DEFINED C_RESTRICT)
+    add_definitions("-Drestrict=")
+    message(STATUS "Restrict keyword - not found")
+  endif(NOT DEFINED C_RESTRICT)
+endmacro(find_restrict_keyword)
