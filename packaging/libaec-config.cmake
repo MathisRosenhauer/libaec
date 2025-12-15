1c22c017c4ae68f2081c4caf4a570252446de6e1
Orion Poplawski <orion@nwra.com>
2025-12-15 09:33:50 -0700

    Split static cmake targets into separate export
    See: https://src.fedoraproject.org/rpms/libaec/pull-request/2

--- packaging/libaec-config.cmake
+++ packaging/libaec-config.cmake
@@ -35,11 +35,10 @@
 #                         AEC library (according to the value of
 #                         libaec_USE_STATIC_LIBS).
 
-include(${CMAKE_CURRENT_LIST_DIR}/libaec-targets.cmake)
-
 # Alias static or shared targets depending on libaec_USE_STATIC_LIBS
 if(libaec_USE_STATIC_LIBS)
-  if(TARGET libaec::aec-static AND TARGET libaec::sz-static AND NOT TARGET libaec::aec AND NOT TARGET libaec::sz)
+  include(${CMAKE_CURRENT_LIST_DIR}/libaec_static-targets.cmake)
+  if(TARGET libaec::aec-static AND TARGET libaec::sz-static AND NOT TARGET lib
     add_library(libaec::aec ALIAS libaec::aec-static)
     add_library(libaec::sz ALIAS libaec::sz-static)
   else()
@@ -48,6 +47,8 @@ if(libaec_USE_STATIC_LIBS)
     set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
   endif()
 else()
+  include(${CMAKE_CURRENT_LIST_DIR}/libaec_shared-targets.cmake)
+
   if(TARGET libaec::aec-shared AND TARGET libaec::sz-shared AND NOT TARGET libaec::aec AND NOT TARGET libaec::sz)
     add_library(libaec::aec ALIAS libaec::aec-shared)
     add_library(libaec::sz ALIAS libaec::sz-shared)
